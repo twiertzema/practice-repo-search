668d793a085f958a27571b11378ae5ced88641fe
Travis Wiertzema <4044268+twiertzema@users.noreply.github.com>
2025-04-09 17:24:08 +0000

    Remove DaisyUI and use HeadlessUI to make dropdown filter

--- src/components/DropdownFilter/component.tsx
+++ src/components/DropdownFilter/component.tsx
@@ -1,3 +1,5 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { type HTMLAttributes, useState } from "react";
 
@@ -18,28 +20,43 @@ export default function DropdownFilter({
 	const [value, setValue] = useState(defaultValue);
 
 	return (
-		<div
-			{...props}
-			className={clsx("dropdown", "dropdown-start", props.className)}
-		>
-			<button className="m1" type="button">
-				{label} <span className="mx-2">{value}</span> &#9660;
-			</button>
-			<ul className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 shadow-sm">
-				{options.map((option) => (
-					// biome-ignore lint/a11y/useKeyWithClickEvents: TODO: handle keyboard input
-					<li
-						className="p-1 hover:bg-base-200"
-						key={option.value}
-						onClick={() => {
-							console.log(option);
-							setValue(option.value);
-						}}
-					>
-						{option.label}
-					</li>
-				))}
-			</ul>
+		<div {...props}>
+			<Menu>
+				<MenuButton
+					className={clsx(
+						"flex w-fit cursor-pointer items-center gap-2 rounded border-1 bg-slate-200 p-2 dark:bg-slate-800",
+						"hover:bg-slate-300 focus:bg-slate-300 dark:focus:bg-slate-700 dark:hover:bg-slate-700",
+					)}
+				>
+					<span>{label}</span>
+
+					<span>{value}</span>
+
+					<ChevronDownIcon className="pointer-events-none size-4" />
+				</MenuButton>
+
+				<MenuItems
+					anchor="bottom start"
+					className={clsx(
+						"w-fit origin-top-left rounded border-1 bg-slate-200 dark:bg-slate-800",
+					)}
+					transition
+				>
+					{options.map((option) => (
+						<MenuItem key={option.value}>
+							{/* biome-ignore lint/a11y/useKeyWithClickEvents: TODO: Support keyboard */}
+							<p
+								className={clsx(
+									"cursor-pointer p-2 hover:bg-slate-300 dark:hover:bg-slate-700",
+								)}
+								onClick={() => setValue(option.value)}
+							>
+								{option.label}
+							</p>
+						</MenuItem>
+					))}
+				</MenuItems>
+			</Menu>
 		</div>
 	);
 }
