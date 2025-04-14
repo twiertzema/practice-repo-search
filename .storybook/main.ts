e64ada766f79470db571d6107c943ff020e47b5b
Travis Wiertzema <4044268+twiertzema@users.noreply.github.com>
2025-04-14 15:24:30 +0000

    Integrated theme toggle into Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,8 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-themes"
   ],
   "framework": {
     "name": "@storybook/react-vite",
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,26 @@
-import type { Preview } from '@storybook/react'
+import { withThemeByClassName } from "@storybook/addon-themes";
+import type { Preview } from "@storybook/react";
+import "../src/tailwind.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
 };
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
 
 export default preview;
